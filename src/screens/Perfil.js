55873c679dd2edf05727b7a6a27942eb647c0c38
Gisele <belagisa14@gmail.com>
2024-06-16 16:53:43 -0300

    add: Perfil

--- src/screens/Perfil.js
+++ src/screens/Perfil.js
@@ -1,9 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import Foto from '../assets/Perfil.jpg'
 
-export default function Perfil() {
+export default function Perfil({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Perfil</Text>
+      <ImageBackground
+        source={require('../assets/Fundo1.png')}
+        style={styles.imageBackground}>
+        <View style={styles.navbar}>
+          <TouchableOpacity onPress={() => navigation.openDrawer()}>
+            <AntDesign name="bars" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.title}>Meu Perfil</Text>
+
+        <View style={styles.formContext}>
+          <View style={styles.imagem}>
+          <Image source={Foto} style={styles.imagem2} />
+          </View>
+
+          <View style={styles.textContainer}>
+            <Text style={styles.textForm}>Nome:</Text>
+            <Text style={styles.input}>Gisele Gomes Oliveira</Text>
+
+            <Text style={styles.textForm}>E-mail:</Text>
+            <Text style={styles.input}>202110446@uesb.edu.br</Text>
+
+            <Text style={styles.textForm}>Tipo de usuário:</Text>
+            <Text style={styles.input}>Administrador</Text>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -11,13 +41,95 @@ export default function Perfil() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    height: "auto"
   },
 
   title: {
+    color: "#FFFFFF",
+    fontSize: 30,
+    textAlign: "center",
+    paddingBottom: 30,
+    marginTop: 50
+  },
+
+  formContext: {
+    backgroundColor: "#FFFFFF",
+    width: "85%",
+    height: "65%",
+    borderRadius: 30,
+    marginBottom: 10,
+    alignItems: "center",
+  },
+
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  buttonCadastrar: {
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "70%",
+    backgroundColor: "#211DFF",
+    paddingTop: 14,
+    paddingBottom: 14,
+    marginLeft: 50,
+    margin: 25,
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+  },
+
+  navbar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 300
+  },
+
+  textForm: {
+    color: "#0805A3",
     fontSize: 22,
-    fontWeight: "bold"
-  }
+    textAlign: "center",
+    marginBottom: 5,
+  },
+
+  input: {
+    width: "90%",
+    borderRadius: 15,
+    backgroundColor: "#ECEBFD",
+    height: 50,
+    margin: 10,
+    paddingTop: 15,
+    textAlign: "center",
+    marginBottom: 30
+  },
+
+  imagem: {
+    marginTop: 30,
+    marginBottom: 20,
+    borderRadius: 150,
+    backgroundColor: "#ECEBFD",
+    width: "41%",
+    height: "24%",
+    alignItems: "center",
+    paddingBottom: 10
+  },
+
+  textContainer: {
+    width: "100%",
+    alignItems: "center",
+  },
+
+  imagem2: {
+    width: 120,
+    height: 120,
+    marginTop: 7,
+    borderRadius: 100,
+},
 });
